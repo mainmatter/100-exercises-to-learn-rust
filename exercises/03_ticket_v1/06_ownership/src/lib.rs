785c9af4be0940c65ec5b2598fb136b5481bca27
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2024-05-13 12:51:20 +0200

    Fix typo in comment.

--- exercises/03_ticket_v1/06_ownership/src/lib.rs
+++ exercises/03_ticket_v1/06_ownership/src/lib.rs
@@ -55,7 +55,7 @@ mod tests {
     fn works() {
         let ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
         // If you change the signatures as requested, this should compile:
-        // we can call these methods one after the after because they borrow `self`
+        // we can call these methods one after the other because they borrow `self`
         // rather than taking ownership of it.
         assert_eq!(ticket.title(), "A title");
         assert_eq!(ticket.description(), "A description");
