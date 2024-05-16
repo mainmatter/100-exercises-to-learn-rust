804b275b06a83fda7552c7844dafa8c770eef669
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2024-05-16 10:04:49 +0200

    Update guidance in 08/04.

--- exercises/08_futures/04_future/src/lib.rs
+++ exercises/08_futures/04_future/src/lib.rs
@@ -1,6 +1,7 @@
 //! TODO: get the code to compile by **re-ordering** the statements
 //!  in the `example` function. You're not allowed to change the
 //!  `spawner` function nor what each line does in `example`.
+//!   You can wrap existing statements in blocks `{}` if needed.
 use std::rc::Rc;
 use tokio::task::yield_now;
 
