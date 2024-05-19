6cb519aa4b22e6183f6ab28a73701fd30ff413d8
Sh099078 <2141539-Sh099078@users.noreply.gitlab.com>
2024-05-19 13:35:34 +0200

    exercise 06_01_arrays: replace custom function by From<&Weekday> for usize implementation
    In the previous parts of the tutorial, the reader is taught about idiomatic ways to do fallible and infallible conversions between types. Later, the exercise 06_01_arrays gives an opportunity to use that knowledge by requiring a conversion between an enum reference `&Weekday` into an usize.
    
    As of now, the `solutions` branch implements a custom `weekday2index` method to do the conversion. It misses an opportunity to show the learners an application of what they learned in the previous chapters about the `From` trait.
    
    Update the solution of exercise 06_01_arrays to use an implementation of the `From` trait to convert `&Weekday` references to `usize` instead of using a `weekday2index` function.

--- exercises/06_ticket_management/01_arrays/src/lib.rs
+++ exercises/06_ticket_management/01_arrays/src/lib.rs
@@ -22,25 +22,27 @@ impl WeekTemperatures {
     }
 
     pub fn get_temperature(&self, day: Weekday) -> Option<i32> {
-        let index = weekday2index(&day);
+        let index = usize::from(&day);
         self.temperatures[index]
     }
 
     pub fn set_temperature(&mut self, day: Weekday, temperature: i32) {
-        let index = weekday2index(&day);
+        let index = usize::from(&day);
         self.temperatures[index] = Some(temperature);
     }
 }
 
-fn weekday2index(day: &Weekday) -> usize {
-    match day {
-        Weekday::Monday => 0,
-        Weekday::Tuesday => 1,
-        Weekday::Wednesday => 2,
-        Weekday::Thursday => 3,
-        Weekday::Friday => 4,
-        Weekday::Saturday => 5,
-        Weekday::Sunday => 6,
+impl From<&Weekday> for usize {
+    fn from(value: &Weekday) -> Self {
+        match value {
+            Weekday::Monday => 0,
+            Weekday::Tuesday => 1,
+            Weekday::Wednesday => 2,
+            Weekday::Thursday => 3,
+            Weekday::Friday => 4,
+            Weekday::Saturday => 5,
+            Weekday::Sunday => 6,
+        }
     }
 }
 
