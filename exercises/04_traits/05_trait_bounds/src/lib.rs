d5c0743c5607e684bfc38ad42cec0b8d5eb562a3
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2024-05-24 12:36:15 +0200

    Reword instructions.

--- exercises/04_traits/05_trait_bounds/src/lib.rs
+++ exercises/04_traits/05_trait_bounds/src/lib.rs
@@ -1,5 +1,5 @@
 // TODO: Add the necessary trait bounds to `min` so that it compiles successfully.
-//   Refer to `std::cmp` for more information on the traits you might need.
+//   Refer to the documentation of the `std::cmp` module for more information on the traits you might need.
 //
 // Note: there are different trait bounds that'll make the compiler happy, but they come with
 // different _semantics_. We'll cover those differences later in the course when we talk about ordered
