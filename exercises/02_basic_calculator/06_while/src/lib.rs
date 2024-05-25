36665e88ca04425ae37ce0dfd05d024cd4dcbe4d
swamptin <swamptin@pm.me>
2024-05-25 23:33:47 +0100

    Finished first 2 chapters

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,16 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut result: u32 = 1;
+    if n == 0{
+        return 1
+    }
+    let mut count = n;
+    while count > 0 {
+        result = result * count;
+        count -= 1;
+    }
+    return result;
 }
 
 #[cfg(test)]
--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,13 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut answer = 1;
+    if n == 0 {
+        return answer;
+    }
+    for i in 1..=n {
+        answer *= i;
+    }
+    return answer
 }
 
 #[cfg(test)]
