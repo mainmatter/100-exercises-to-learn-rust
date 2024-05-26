32fe3a11cd980b08c82e02b5fccb356f75fcd532
Danh <danhvo.uit@gmail.com>
2024-05-26 06:22:49 +0000

    [0304] ticket_v1 - visibility

--- exercises/03_ticket_v1/04_visibility/src/lib.rs
+++ exercises/03_ticket_v1/04_visibility/src/lib.rs
@@ -1,5 +1,5 @@
-mod ticket {
-    struct Ticket {
+pub mod ticket {
+    pub struct Ticket {
         title: String,
         description: String,
         status: String,
@@ -43,35 +43,35 @@ mod tests {
     // Be careful though! We don't want this function to compile after you have changed
     // visibility to make the use statement compile!
     // Once you have verified that it indeed doesn't compile, comment it out.
-    fn should_not_be_possible() {
-        let ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
+    // fn should_not_be_possible() {
+    //     let ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
 
-        // You should be seeing this error when trying to run this exercise:
-        //
-        // error[E0616]: field `description` of struct `encapsulation::ticket::Ticket` is private
-        //    |
-        //    |              assert_eq!(ticket.description, "A description");
-        //    |                         ^^^^^^^^^^^^^^^^^^
-        //
-        // TODO: Once you have verified that the below does not compile,
-        //   comment the line out to move on to the next exercise!
-        assert_eq!(ticket.description, "A description");
-    }
+    //     // You should be seeing this error when trying to run this exercise:
+    //     //
+    //     // error[E0616]: field `description` of struct `encapsulation::ticket::Ticket` is private
+    //     //    |
+    //     //    |              assert_eq!(ticket.description, "A description");
+    //     //    |                         ^^^^^^^^^^^^^^^^^^
+    //     //
+    //     // TODO: Once you have verified that the below does not compile,
+    //     //   comment the line out to move on to the next exercise!
+    //     // assert_eq!(ticket.description, "A description");
+    // }
 
-    fn encapsulation_cannot_be_violated() {
-        // This should be impossible as well, with a similar error as the one encountered above.
-        // (It will throw a compilation error only after you have commented the faulty line
-        // in the previous test - next compilation stage!)
-        //
-        // This proves that `Ticket::new` is now the only way to get a `Ticket` instance.
-        // It's impossible to create a ticket with an illegal title or description!
-        //
-        // TODO: Once you have verified that the below does not compile,
-        //   comment the lines out to move on to the next exercise!
-        let ticket = Ticket {
-            title: "A title".into(),
-            description: "A description".into(),
-            status: "To-Do".into(),
-        };
-    }
+    //fn encapsulation_cannot_be_violated() {
+    //    // This should be impossible as well, with a similar error as the one encountered above.
+    //    // (It will throw a compilation error only after you have commented the faulty line
+    //    // in the previous test - next compilation stage!)
+    //    //
+    //    // This proves that `Ticket::new` is now the only way to get a `Ticket` instance.
+    //    // It's impossible to create a ticket with an illegal title or description!
+    //    //
+    //    // TODO: Once you have verified that the below does not compile,
+    //    //   comment the lines out to move on to the next exercise!
+    //    let ticket = Ticket {
+    //        title: "A title".into(),
+    //        description: "A description".into(),
+    //        status: "To-Do".into(),
+    //    };
+    //}
 }
