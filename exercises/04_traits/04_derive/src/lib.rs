0c5e9e790b0a95c2eb9c50e40cc59cdf3f99018a
Danh <danhvo.uit@gmail.com>
2024-05-27 16:21:28 +0000

    [0404] traits - derive

--- exercises/04_traits/04_derive/src/lib.rs
+++ exercises/04_traits/04_derive/src/lib.rs
@@ -9,6 +9,7 @@
 // If the compared type doesn't implement `Debug`, it doesn't know how to represent them!
 
 #[derive(PartialEq)]
+#[derive(Debug)]
 struct Ticket {
     title: String,
     description: String,
