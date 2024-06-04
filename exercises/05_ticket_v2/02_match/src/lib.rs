48a374fb1708f57533c9f29296d6f826ea23f25e
Danh <danhvo.uit@gmail.com>
2024-06-04 15:24:06 +0000

    [0502] ticket_v2 - match

--- exercises/05_ticket_v2/02_match/src/lib.rs
+++ exercises/05_ticket_v2/02_match/src/lib.rs
@@ -9,7 +9,13 @@ enum Shape {
 impl Shape {
     // TODO: Implement the `n_sides` method using a `match`.
     pub fn n_sides(&self) -> u8 {
-        todo!()
+        match self {
+            Self::Circle => 0,
+            Self::Square => 4,
+            Self::Rectangle => 4,
+            Self::Triangle => 3,
+            Self::Pentagon => 5
+        }
     }
 }
 
