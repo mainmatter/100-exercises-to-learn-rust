bc39049b7acd81c23e253f5cded7cda85064a47f
One <43485962+c-git@users.noreply.github.com>
2024-06-06 14:30:57 -0400

    Change test to require impl
    impl std::ops::Add<&SaturatingU16> for SaturatingU16

--- exercises/04_traits/14_outro/tests/integration.rs
+++ exercises/04_traits/14_outro/tests/integration.rs
@@ -6,11 +6,12 @@ fn test_saturating_u16() {
     let b: SaturatingU16 = 5u8.into();
     let c: SaturatingU16 = u16::MAX.into();
     let d: SaturatingU16 = (&1u16).into();
+    let e = &c;
 
     assert_eq!(a + b, SaturatingU16::from(15u16));
     assert_eq!(a + c, SaturatingU16::from(u16::MAX));
     assert_eq!(a + d, SaturatingU16::from(11u16));
     assert_eq!(a + a, 20u16);
     assert_eq!(a + 5u16, 15u16);
-    assert_eq!(a + &u16::MAX, SaturatingU16::from(u16::MAX));
+    assert_eq!(a + e, SaturatingU16::from(u16::MAX));
 }
