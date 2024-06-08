1d333edc25258912d83c21ae96d53d53bca5c33b
Danh <danhvo.uit@gmail.com>
2024-06-08 09:02:58 +0000

    [0513] ticket_v2 - try_from

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -8,6 +8,36 @@ enum Status {
     Done,
 }
 
+impl TryFrom<String> for Status {
+    type Error = ();
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if value.to_lowercase() == "todo" {
+            Ok(Status::ToDo)
+        } else if value.to_lowercase() == "inprogress" {
+            Ok(Status::InProgress)
+        } else if value.to_lowercase() == "done" {
+            Ok(Status::Done)
+        } else {
+            Err(())
+        }
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = ();
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if String::from(value).to_lowercase() == "todo" {
+            Ok(Status::ToDo)
+        } else if String::from(value).to_lowercase() == "inprogress" {
+            Ok(Status::InProgress)
+        } else if String::from(value).to_lowercase() == "done" {
+            Ok(Status::Done)
+        } else {
+            Err(())
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
