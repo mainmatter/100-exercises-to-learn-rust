5295d2a3911e00a562738a4787d1d7a6b3a7f789
gn-t-k <gntk1995422@gmail.com>
2024-07-01 06:26:01 +0900

    02/05

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -10,6 +10,14 @@
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
 
+fn factorial(n: u32) -> u32 {
+    if n == 0 {
+        return 1;
+    }
+
+    n * factorial(n - 1)
+}
+
 #[cfg(test)]
 mod tests {
     use crate::factorial;
