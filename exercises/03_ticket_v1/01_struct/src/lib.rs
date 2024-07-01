48fdfc3ed23a00e92077367c2f2d8019ebf42733
nyakiomaina <carolinenyakio21@gmail.com>
2024-07-01 21:26:20 +0300

    struct exercise

--- exercises/03_ticket_v1/01_struct/src/lib.rs
+++ exercises/03_ticket_v1/01_struct/src/lib.rs
@@ -1,9 +1,22 @@
 // Define a struct named `Order` with the following fields:
 // - `price`, an unsigned integer
 // - `quantity`, an unsigned integer
+struct Order {
+    price: u32,
+    quantity: u32,
+}
 //
 // It should also have a method named `is_available` that returns a `true` if the quantity is
 // greater than 0, otherwise `false`.
+impl Order {
+    fn is_available(&self) -> bool {
+        if self.quantity  > 0 {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
