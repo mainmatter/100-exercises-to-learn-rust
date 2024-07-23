1c9484e88bc70c48aebd336833452e3b318611f4
leolucabernardi <83414821+leolucabernardi@users.noreply.github.com>
2024-07-23 12:58:26 +0200

    0205: factorial: not facotrial :)

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -9,7 +9,12 @@
 // `factorial(2)` to return `2`, and so on.
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
-
+fn factorial(n: u32) -> u32 {
+    if n == 0 {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
 #[cfg(test)]
 mod tests {
     use crate::factorial;
