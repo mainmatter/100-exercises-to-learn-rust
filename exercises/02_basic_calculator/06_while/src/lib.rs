b2844184f82333a57181ce74a43454a0f903ac8c
leolucabernardi <83414821+leolucabernardi@users.noreply.github.com>
2024-07-23 13:02:50 +0200

    0206: while: basics basics basics

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,13 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut x: u32 = 1;
+    let mut i: u32 = n;
+    while i > 0 {
+        x *= i;
+        i -= 1;
+    }
+    return x;
 }
 
 #[cfg(test)]
