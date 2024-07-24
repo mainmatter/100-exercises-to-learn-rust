d4c285127eeb7b47444b86bff63e3a697d63ea69
geewynn <ekainugodwin@gmail.com>
2024-07-24 09:19:06 +0100

    04 exercise solutions

--- exercises/04_traits/05_trait_bounds/src/lib.rs
+++ exercises/04_traits/05_trait_bounds/src/lib.rs
@@ -5,8 +5,10 @@
 // different _semantics_. We'll cover those differences later in the course when we talk about ordered
 // collections (e.g. BTreeMap).
 
+use std::fmt::Debug;
+
 /// Return the minimum of two values.
-pub fn min<T>(left: T, right: T) -> T {
+pub fn min<T: PartialOrd>(left: T, right: T) -> T {
     if left <= right {
         left
     } else {
