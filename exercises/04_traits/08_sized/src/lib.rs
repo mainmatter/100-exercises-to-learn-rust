b88df80a96aba0a4989b13012f282872b51193e6
Calvin Lee <jinwoo0451@naver.com>
2024-07-28 20:18:00 +0900

    04-09

--- exercises/04_traits/08_sized/src/lib.rs
+++ exercises/04_traits/08_sized/src/lib.rs
@@ -1,7 +1,21 @@
-pub fn example() {
+pub fn example() -> usize {
     // Trying to get the size of a str (or any other DST)
     // via `std::mem::size_of` will result in a compile-time error.
     //
     // TODO: Comment out the following line and move on to the next exercise.
-    std::mem::size_of::<str>();
+    // str은 컴파일타임에 사이즈 알 수 없음 (DST)
+    // &str은 알 수 있음(&str)
+    let res = std::mem::size_of::<&str>();
+    res
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test() {
+//         let res = example();
+//         print!("{:?}", res);
+//     }
+// }
--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -4,7 +4,25 @@ pub struct WrappingU32 {
     value: u32,
 }
 
+impl From<u32> for WrappingU32 {
+    fn from(value: u32) -> Self {
+        WrappingU32 { value: value }
+    }
+}
+
 fn example() {
     let wrapping: WrappingU32 = 42.into();
     let wrapping = WrappingU32::from(42);
+    println!("{:?}", wrapping.value);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        example();
+    }
+}
+
