a8cb46be1a2607591ae4df5c9f9ea530ca47c03a
JuanGaljoen <juangaljoen@gmail.com>
2024-09-17 16:17:48 +0200

    03_ticket

--- exercises/03_ticket_v1/04_visibility/src/lib.rs
+++ exercises/03_ticket_v1/04_visibility/src/lib.rs
@@ -1,12 +1,12 @@
 mod ticket {
-    struct Ticket {
+    pub struct Ticket {
         title: String,
         description: String,
         status: String,
     }
 
     impl Ticket {
-        fn new(title: String, description: String, status: String) -> Ticket {
+        pub fn new(title: String, description: String, status: String) -> Ticket {
             if title.is_empty() {
                 panic!("Title cannot be empty");
             }
@@ -36,6 +36,7 @@ mod ticket {
 //  in this exercise.
 #[cfg(test)]
 mod tests {
+
     // TODO: Add the necessary `pub` modifiers in the parent module to remove the compiler
     //  errors about the use statement below.
     use super::ticket::Ticket;
@@ -55,7 +56,7 @@ mod tests {
         //
         // TODO: Once you have verified that the below does not compile,
         //   comment the line out to move on to the next exercise!
-        assert_eq!(ticket.description, "A description");
+        // assert_eq!(ticket.description, "A description");
     }
 
     fn encapsulation_cannot_be_violated() {
@@ -68,10 +69,10 @@ mod tests {
         //
         // TODO: Once you have verified that the below does not compile,
         //   comment the lines out to move on to the next exercise!
-        let ticket = Ticket {
-            title: "A title".into(),
-            description: "A description".into(),
-            status: "To-Do".into(),
-        };
+        // let ticket = Ticket {
+        //     title: "A title".into(),
+        //     description: "A description".into(),
+        //     status: "To-Do".into(),
+        // };
     }
 }
--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -1,13 +1,75 @@
-// TODO: Define a new `Order` type.
-//   It should keep track of three pieces of information: `product_name`, `quantity`, and `unit_price`.
-//   The product name can't be empty and it can't be longer than 300 bytes.
-//   The quantity must be strictly greater than zero.
-//   The unit price is in cents and must be strictly greater than zero.
-//   Order must include a method named `total` that returns the total price of the order.
-//   Order must provide setters and getters for each field.
-//
-// Tests are located in a different place this time—in the `tests` folder.
-// The `tests` folder is a special location for `cargo`. It's where it looks for **integration tests**.
-// Integration here has a very specific meaning: they test **the public API** of your project.
-// You'll need to pay attention to the visibility of your types and methods; integration
-// tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        Self::validate_product_name(&product_name);
+        Self::validate_quantity(quantity);
+        Self::validate_unit_price(unit_price);
+
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    // Getters
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> u32 {
+        self.quantity
+    }
+
+    pub fn unit_price(&self) -> u32 {
+        self.unit_price
+    }
+
+    // Setters
+    pub fn set_product_name(&mut self, product_name: String) {
+        Self::validate_product_name(&product_name);
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        Self::validate_quantity(quantity);
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        Self::validate_unit_price(unit_price);
+        self.unit_price = unit_price;
+    }
+
+    // Validation methods
+    fn validate_product_name(product_name: &String) {
+        if product_name.is_empty() {
+            panic!("Product name can't be empty");
+        }
+
+        if product_name.len() > 300 {
+            panic!("Product name can't be longer than 300 bytes");
+        }
+    }
+
+    fn validate_quantity(quantity: u32) {
+        if quantity == 0 {
+            panic!("Quantity must be greater than 0");
+        }
+    }
+
+    fn validate_unit_price(unit_price: u32) {
+        if unit_price == 0 {
+            panic!("Unit price must be greater than 0");
+        }
+    }
+}
