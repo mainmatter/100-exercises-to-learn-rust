f8d5782ae044e219ad124fc2dab56ab4c94f392b
Felix Yu <f.yu@acatis-research.de>
2024-10-22 09:54:10 +0200

    06_while

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,19 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    if n==0
+    {
+        return 1;
+    }
+    let mut result=1;
+    let mut i=1;
+    while n>=i
+    {
+        result*=i;
+        i+=1;
+    }
+
+    result
 }
 
 #[cfg(test)]
