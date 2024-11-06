d2c7f5f085cf064002426a7f9a9d519d392c5321
smartdev <smartdevs17@gmail.com>
2024-11-07 00:31:10 +0100

    update visibility modifiers

--- exercises/03_ticket_v1/04_visibility/src/lib.rs
+++ exercises/03_ticket_v1/04_visibility/src/lib.rs
@@ -1,12 +1,12 @@
 mod ticket {
-    struct Ticket {
-        title: String,
-        description: String,
-        status: String,
+   pub struct Ticket {
+       pub(crate) title: String,
+       pub(crate) description: String,
+       pub(crate) status: String,
     }
 
     impl Ticket {
-        fn new(title: String, description: String, status: String) -> Ticket {
+       pub fn new(title: String, description: String, status: String) -> Ticket {
             if title.is_empty() {
                 panic!("Title cannot be empty");
             }
