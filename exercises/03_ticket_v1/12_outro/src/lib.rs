0147d990dd75e11c863f6615e33799cb3fa746d2
Alt5r <rrow8nn@gmail.com>
2024-11-12 22:30:53 +0000

    resercieses 3

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,94 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name:String,
+    quantity:u16,
+    unit_price:u16
+}
+impl Order {
+
+
+
+// getters
+
+pub fn product_name(&self) -> &String {
+    return &self.product_name
+}
+
+pub fn quantity(&self) -> &u16 {
+    return &self.quantity
+}
+
+pub fn unit_price(&self) -> &u16 {
+    return &self.unit_price
+}
+
+pub fn total(&self) -> u16 {
+    return &self.unit_price * &self.quantity
+}
+
+// validation
+
+fn name_val(name:&String) -> bool {
+    if name.is_empty() == false && name.len() < 300 {
+        return true
+    } else {
+        return false
+    }
+}
+
+fn quant_val(qant:&u16) -> bool {
+    return qant > &0
+}
+
+fn unit_val(unit:&u16) -> bool {
+    return unit>&0
+}
+
+// setters
+
+pub fn set_product_name(&mut self, new_name:String) { 
+    if Self::name_val(&new_name) == false {
+        panic!("Some shit here")
+    } else {
+        self.product_name = new_name;
+    }
+}
+
+pub fn set_quantity(&mut self, new_quant:u16) {
+    if Self::quant_val(&new_quant) == false {
+        panic!("Do some shit here")
+
+    } else {
+        self.quantity = new_quant;
+    }
+}
+pub fn set_unit_price(&mut self, new_price:u16) {
+    if Self::unit_val(&new_price) == false {
+        panic!("Do some shit here")
+    } else {
+        self.unit_price = new_price;
+    }
+}
+// new
+
+pub fn new(product_name:String, quantity:u16, unit_price:u16) -> Order {
+    if Self::name_val(&product_name) == false {
+        panic!("Some shit here")
+    }
+    if Self::quant_val(&quantity) == false {
+        panic!("Do some shit here")
+
+    }
+    if Self::unit_val(&unit_price) == false {
+        panic!("Do some shit here")
+    }
+
+    Order {
+        product_name,
+        quantity,
+        unit_price
+    }
+}
+}
--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -2,7 +2,23 @@
 // even, otherwise `false`.
 //
 // Then implement the trait for `u32` and `i32`.
+trait IsEven {
+    fn is_even(self) -> bool;
+}
+impl IsEven for u32 {
+    fn is_even(self) -> bool {
+        self % 2 == 0
+    }
+}
 
+trait IsEven2 {
+    fn is_even(self) -> bool;
+}
+impl IsEven2 for i32 {
+    fn is_even(self) -> bool {
+        self % 2 == 0
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
