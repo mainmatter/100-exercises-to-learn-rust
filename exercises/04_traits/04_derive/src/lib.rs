476347a6edf5d38e66a99af12cd20aa6a9e09501
Alt5r <rrow8nn@gmail.com>
2024-11-19 22:10:38 +0000

    s

--- exercises/04_traits/04_derive/src/lib.rs
+++ exercises/04_traits/04_derive/src/lib.rs
@@ -4,10 +4,10 @@
 // # `Debug` primer
 //
 // `Debug` returns a representation of a Rust type that's suitable for debugging (hence the name).
-// `assert_eq!` requires `Ticket` to implement `Debug` because, when the assertion fails, it tries to
+// `assert_eq !` requires `Ticket` to implement `Debug` because, when the assertion fails, it tries to
 // print both sides of the comparison to the terminal.
 // If the compared type doesn't implement `Debug`, it doesn't know how to represent them!
-
+ 
 #[derive(PartialEq)]
 struct Ticket {
     title: String,
