397c6452d2fcffc98b0d1a875d78d11d4c6f2494
iuxtaa <anngabrielledelrosario@gmail.com>
2024-12-06 15:36:22 +1300

    feat: finish 02_basic_calculator/04_panics

--- exercises/02_basic_calculator/04_panics/src/lib.rs
+++ exercises/02_basic_calculator/04_panics/src/lib.rs
@@ -1,9 +1,14 @@
+use core::time;
+
 /// Given the start and end points of a journey, and the time it took to complete the journey,
 /// calculate the average speed of the journey.
 fn speed(start: u32, end: u32, time_elapsed: u32) -> u32 {
     // TODO: Panic with a custom message if `time_elapsed` is 0
-
-    (end - start) / time_elapsed
+    if time_elapsed == 0 { 
+        panic!("The journey took no time at all. That's impossible!")
+    } else {
+        (end - start) / time_elapsed
+    }
 }
 
 #[cfg(test)]
