94de04b2bd7f3d93a3b7c492d5c4d16338d73ef8
iuxtaa <anngabrielledelrosario@gmail.com>
2024-12-06 16:16:03 +1300

    feat: finish 02_basic_calculator/09_saturating

--- exercises/02_basic_calculator/09_saturating/src/lib.rs
+++ exercises/02_basic_calculator/09_saturating/src/lib.rs
@@ -1,9 +1,10 @@
 pub fn factorial(n: u32) -> u32 {
-    let mut result = 1;
+    let mut result: u32 = 1;
     for i in 1..=n {
         // Use saturating multiplication to stop at the maximum value of u32
         // rather than overflowing and wrapping around
-        result *= i;
+        // result *= i;
+        result = result.saturating_mul(i);
     }
     result
 }
