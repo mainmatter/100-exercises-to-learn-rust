17388f9e3013f9dbd494a7eb9b982d350d278719
iuxtaa <ann.delrosario@resideo.com>
2024-12-09 12:17:38 +1300

    feat: finish 05_ticket_v2/_02_match

--- exercises/05_ticket_v2/02_match/src/lib.rs
+++ exercises/05_ticket_v2/02_match/src/lib.rs
@@ -9,7 +9,13 @@ enum Shape {
 impl Shape {
     // TODO: Implement the `n_sides` method using a `match`.
     pub fn n_sides(&self) -> u8 {
-        todo!()
+        match self {
+            Shape::Circle => 0,
+            Shape::Square => 4,
+            Shape::Rectangle => 4,
+            Shape::Triangle => 3,
+            Shape::Pentagon => 5,
+        }
     }
 }
 
