5e6d7f783dd342d975cce54e5689dd02d9e227c1
iuxtaa <ann.delrosario@resideo.com>
2024-12-09 10:15:24 +1300

    feat: finish 04_traits/02_orphan_rule

--- exercises/04_traits/02_orphan_rule/src/lib.rs
+++ exercises/04_traits/02_orphan_rule/src/lib.rs
@@ -4,8 +4,34 @@
 //  Look at the compiler error to get familiar with what it looks like.
 //  Then delete the code below and move on to the next exercise.
 
-impl PartialEq for u32 {
-    fn eq(&self, _other: &Self) -> bool {
-        todo!()
-    }
-}
+/*
+    Extension trait:
+    An extension trait is a trait whose primary purpose is to attach new methods to
+    foreign types, such as u32. That's exactly the pattern you deployed in the previous 
+    exercise, by defining the IsEven trait and the implementing it for i32 and u32. You 
+    are then free to call is_even on those types as long as the IsEven trait is in scope.
+
+    One implementation:
+    You can't implement the same trait twice, in a crate, for the same type.
+
+    Orphan rule:
+    At least one of the following must be true:
+    - The trait is defined in the current crate
+    - The implementor type is defined in the current crate
+
+*/
+
+// impl PartialEq for u32 {
+//     fn eq(&self, _other: &Self) -> bool {
+//         todo!()
+//     }
+// }
+
+/*
+    This gives an error:
+    only traits defined in the current crate can be implemented for primitive types
+    impl doesn't use only types from inside the current crate
+    u32 is not defined in the current crate
+    note: define and implement types from inside the current crate
+
+*/
