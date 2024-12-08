457dc9c34abcfbd9d3bf39103600ec33a5ff17da
iuxtaa <ann.delrosario@resideo.com>
2024-12-09 10:06:51 +1300

    feat: finish 04_traits/01_trait

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -3,6 +3,24 @@
 //
 // Then implement the trait for `u32` and `i32`.
 
+trait IsEven {
+    fn is_even(self) -> bool;
+}
+
+impl IsEven for u32 {
+    fn is_even(self) -> bool {
+        if(self % 2 == 0) { true }
+        else { false }
+    }
+}
+
+impl IsEven for i32 {
+    fn is_even(self) -> bool {
+        if(self % 2 == 0) { true }
+        else { false }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
