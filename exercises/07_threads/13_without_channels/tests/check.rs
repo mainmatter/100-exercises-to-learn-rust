3fb054078d3289d0c0cd52c120d4b6d8d44932f2
iuxtaa <ann.delrosario@resideo.com>
2024-12-11 15:36:14 +1300

    feat: finish 07_threads/13_without_channels

--- exercises/07_threads/13_without_channels/tests/check.rs
+++ exercises/07_threads/13_without_channels/tests/check.rs
@@ -7,7 +7,7 @@ use without_channels::store::TicketStore;
 
 #[test]
 fn works() {
-    let store = todo!();
+    let store = Arc::new(RwLock::new(TicketStore::new()));
 
     let store1 = store.clone();
     let client1 = spawn(move || {
