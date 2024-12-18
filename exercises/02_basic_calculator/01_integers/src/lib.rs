a2b99975131f81110bad967da5d393d6b6cba16a
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2024-12-18 17:36:38 +0100

    Avoid using a suffix

--- exercises/02_basic_calculator/01_integers/src/lib.rs
+++ exercises/02_basic_calculator/01_integers/src/lib.rs
@@ -1,6 +1,7 @@
 fn compute(a: u32, b: u32) -> u32 {
     // TODO: change the line below to fix the compiler error and make the tests pass.
-    a + b * 4u8
+    let multiplier: u8 = 4;
+    a + b * multiplier
 }
 
 #[cfg(test)]
