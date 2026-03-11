51ebbd410dfe7114cd5ddd48ff11fdbf19819555
Vladislav Vysokikh <vvysokikh@gmail.com>
2026-03-11 11:08:58 +0000

    nix devshell setup

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "100 Exercises to Learn Rust - dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" "clippy" "rustfmt" ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+          ];
+
+          shellHook = ''
+            echo "Rust dev environment ready"
+            echo "  rustc: $(rustc --version)"
+            echo "  cargo: $(cargo --version)"
+            exec zsh
+          '';
+        };
+      });
+}
