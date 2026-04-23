fa7b2c2c4ece365dfa5c68c7b531e007dc1980cb
flexii <132218554+Flexii2000@users.noreply.github.com>
2026-04-23 11:35:07 +0200

    fix typo

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -13,7 +13,7 @@
 pub fn fibonacci(n: u32) -> u32 {
     // TODO: implement the `fibonacci` function
     //
-    // Hint: use a `Vec` to memoize the results you have already calculated
+    // Hint: use a `Vec` to memorize the results you have already calculated
     // so that you don't have to recalculate them several times.
     todo!()
 }
