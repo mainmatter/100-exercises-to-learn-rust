033e01c5ed77c966d85b905bbc93574263cbe260
frabera <Francesco.berardinucci@hotmail.it>
2026-06-07 23:44:46 +0200

    Update lib.rs: move is unnecessary

--- exercises/07_threads/02_static/src/lib.rs
+++ exercises/07_threads/02_static/src/lib.rs
@@ -7,8 +7,8 @@ pub fn sum(slice: &'static [i32]) -> i32 {
     let mid = slice.len() / 2;
     let (slice1, slice2) = slice.split_at(mid);
 
-    let handle1 = thread::spawn(move || slice1.iter().sum::<i32>());
-    let handle2 = thread::spawn(move || slice2.iter().sum::<i32>());
+    let handle1 = thread::spawn(|| slice1.iter().sum::<i32>());
+    let handle2 = thread::spawn(|| slice2.iter().sum::<i32>());
 
     handle1.join().unwrap() + handle2.join().unwrap()
 }
